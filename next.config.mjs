7c2065588dec222c4e25d4ebc0583f461f574e87
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-06-14 13:24:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxSize: 24000000, // 24 MiB, just under Cloudflare's 25 MiB limit
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
