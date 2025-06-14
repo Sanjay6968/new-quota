5d1130dde0fe9b707f17cc3845ec57e2135f0a06
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-06-14 14:36:08 +0530

    Clean build: external SVG optimized

--- components/UploadIllustration.tsx
+++ components/UploadIllustration.tsx
@@ -1,5 +1,10 @@
 export default function UploadIllustration() {
   return (
-    <img src="/upload-illustration.svg" alt="Upload illustration" />
+    <img
+      src="/upload-illustration.svg"
+      alt="Upload illustration"
+      loading="lazy"
+      style={{ maxWidth: '100%', height: 'auto' }}
+    />
   );
 }
