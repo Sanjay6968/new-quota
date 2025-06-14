496727f95f450a81fb8cb1f3e5cee924cbcda348
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-06-14 14:15:28 +0530

    Use external SVG with <img> to reduce bundle size

--- components/UploadIllustration.tsx
+++ components/UploadIllustration.tsx
@@ -1,12 +1,5 @@
-import Image from 'next/image';
-
 export default function UploadIllustration() {
   return (
-    <Image
-      src="/upload-illustration.svg"
-      alt="Upload Illustration"
-      width={665}
-      height={968}
-    />
+    <img src="/upload-illustration.svg" alt="Upload illustration" />
   );
 }
