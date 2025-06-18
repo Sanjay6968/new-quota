482d3104667c790917fc9577e639e3cf97e7cf28
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-06-18 11:31:05 +0530

    Update contact.tsx

--- pages/dashboard/contact.tsx
+++ pages/dashboard/contact.tsx
@@ -1,7 +1,7 @@
 // pages/contact.tsx
 
 import React from 'react';
-import ContactPage from '../views/ContactPage/index';
+import ContactPage from '../../views/ContactPage/index';
 
 export default function Contact() {
   return <ContactPage />;
