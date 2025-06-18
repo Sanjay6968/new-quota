99f2a2c97f62b082abbc3138856c579082bfd343
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-06-18 11:50:07 +0530

    Update index.tsx

--- views/ContactPage/index.tsx
+++ views/ContactPage/index.tsx
@@ -1,14 +1,28 @@
 // views/ContactPage/index.tsx
 
 import React from 'react';
+import styled from 'styled-components';
 import FormSection from './FormSection';
 import InformationSection from './InformationSection';
 
 export default function ContactPage() {
   return (
-    <div>
+    <ContactWrapper>
       <InformationSection />
       <FormSection />
-    </div>
+    </ContactWrapper>
   );
 }
+
+const ContactWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 3rem;
+  padding: 4rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 2rem;
+  }
+`;
