0fbf29d869a3ea768f994d1993afe90bfc8040e2
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-07-10 23:19:40 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import 'swiper/css/autoplay';
 
 import { TrackingHeadScript } from "@phntms/next-gtm";
 import { AppProps } from 'next/dist/shared/lib/router/router';
-import dynamic from 'next/dynamic';
 import { Poppins } from 'next/font/google';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
@@ -13,7 +12,6 @@ import { useRouter } from 'next/router';
 import { DefaultSeo } from 'next-seo';
 import { ColorModeScript } from 'nextjs-color-mode';
 import React, { PropsWithChildren } from 'react';
-import { TinaEditProvider } from 'tinacms/dist/edit-state';
 
 import Footer from '@/components/Footer';
 import { GlobalStyle } from '@/components/GlobalStyles';
@@ -30,8 +28,6 @@ const poppins = Poppins({
   display: 'swap',
 });
 
-const TinaCMS = dynamic(() => import('tinacms'), { ssr: false });
-
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -43,25 +39,19 @@ function MyApp({ Component, pageProps }: AppProps) {
       const accessToken = localStorage.getItem('accessToken');
       setIsAuthenticated(!!accessToken);
     };
-  
+
     checkAuth();
-  
-    // Listen for storage changes across tabs/windows
+
     window.addEventListener('storage', checkAuth);
-  
-    return () => {
-      window.removeEventListener('storage', checkAuth);
-    };
+    return () => window.removeEventListener('storage', checkAuth);
   }, [router.pathname]);
 
   const handleSignOut = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
-
-    window.dispatchEvent(new Event('storage')); // Trigger storage event
-
+    window.dispatchEvent(new Event('storage'));
     setIsAuthenticated(false);
-    router.push('/dashboard/sign-in'); // Redirect to sign-in page
+    router.push('/dashboard/sign-in');
   };
 
   const navItems: NavItems = [
@@ -70,8 +60,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     { title: 'Track Order', href: '/tracking', target: '_blank' },
     { title: 'Order History', href: '/order-history' },
     isAuthenticated
-    ? { title: 'Sign Out', href: '/dashboard/sign-in', onClick: handleSignOut, bordered: true}
-    : { title: 'Sign In', href: '/dashboard/sign-in', bordered: true },
+      ? { title: 'Sign Out', href: '/dashboard/sign-in', onClick: handleSignOut, bordered: true }
+      : { title: 'Sign In', href: '/dashboard/sign-in', bordered: true },
     { title: 'STORE', href: 'https://store.mekuva.com', outlined: true, target: '_blank' },
   ];
 
@@ -93,26 +83,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <GlobalStyle />
         <TrackingHeadScript id={GA_TRACKING_ID} isGTM={true} />
         <DefaultSeo {...SEO} />
+
         <Providers navItems={navItems}>
           {!isExcludedRoute && <Navbar items={navItems} />}
-          
-          <TinaEditProvider
-            editMode={
-              <TinaCMS
-                query={pageProps.query}
-                variables={pageProps.variables}
-                data={pageProps.data}
-                isLocalClient={!process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
-                branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
-                clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
-                {...pageProps}
-              >
-                {(livePageProps: any) => <Component {...livePageProps} />}
-              </TinaCMS>
-            }
-          >
-            <Component {...pageProps} />
-          </TinaEditProvider>
+          <Component {...pageProps} />
           {!isExcludedRoute && <WaveCta />}
           {!isExcludedRoute && <Footer />}
         </Providers>
